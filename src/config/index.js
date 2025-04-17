0c8bb78e0f337d70fbdd30d0d002400c43436292
hanaravind <hanaravind5@gmail.com>
2025-04-18 02:28:18 +0530

    be url updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 // const API_URL = import.meta.env.API_BASE_URL;
 // export { API_URL };
 
-export const API_URL = 'https://backend-assistant-9g84.onrender.com'
+export const API_URL = 'https://backend-assistant-9g84.onrender.com/api'
